c10c609caced907d96e15e6d835cfb1a314861d7
Szymon Wieczorek <doszymona@gmail.com>
2025-03-19 20:31:58 +0100

    get rid off tables

--- count_proper_name_sentences.py
+++ count_proper_name_sentences.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 count_proper_name_sentences.py - Liczy procent zdań, które zawierają przynajmniej jedną nazwę własną.
 Nazwą własną jest każdy wyraz napisany wielką literą, nie będący pierwszym wyrazem w zdaniu.
 """
 import sys
+# Ustawienie kodowania dla wejścia/wyjścia standardowego
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
@@ -12,69 +15,90 @@ def is_end_of_sentence(char):
     """
     return char in '.!?'
 
+def is_whitespace(char):
+    """
+    Sprawdza, czy znak jest białym znakiem.
+    """
+    return char in ' \t\n\r\f\v'
+
 def has_proper_name(sentence):
     """
     Sprawdza, czy zdanie zawiera nazwę własną.
+    Używa przetwarzania znak po znaku bez używania list.
     """
-    words = []
-    word = ""
+    in_word = False
+    is_first_word = True
+    current_word_has_uppercase = False
     
     for char in sentence:
-        if char in ' \t\n\r\f\v':
-            if word:
-                words.append(word)
-                word = ""
+        if is_whitespace(char):
+            if in_word:
+                # Koniec słowa
+                if current_word_has_uppercase and not is_first_word:
+                    return True
+                is_first_word = False
+                in_word = False
+                current_word_has_uppercase = False
         else:
-            word += char
+            if not in_word:
+                # Początek nowego słowa
+                in_word = True
+                if char.isupper():
+                    current_word_has_uppercase = True
+            # Kontynuacja słowa - nic nie robimy
     
-    if word:
-        words.append(word)
-    
-    # Sprawdzamy, czy istnieje wyraz z wielką literą, który nie jest pierwszym wyrazem zdania
-    for i in range(1, len(words)):
-        word = words[i]
-        if word and word[0].isupper():
-            return True
+    # Sprawdzenie ostatniego słowa w zdaniu, jeśli istnieje
+    if in_word and current_word_has_uppercase and not is_first_word:
+        return True
     
     return False
 
 def count_sentences_with_proper_names():
     """
     Liczy procent zdań, które zawierają przynajmniej jedną nazwę własną.
+    Używa przetwarzania znak po znaku bez używania list.
     """
-    text = sys.stdin.read()
-    
-    sentences = []
-    sentence = ""
+    # Zmienne pomocnicze do przetwarzania tekstu
+    current_sentence = ""
     in_sentence = False
+    proper_name_count = 0
+    total_sentences = 0
     
-    for i in range(len(text)):
-        char = text[i]
-        
-        if not in_sentence and not (char in ' \t\n\r\f\v'):
+    # Czytamy tekst znak po znaku
+    char = sys.stdin.read(1)
+    prev_char = None
+    
+    while char:
+        # Sprawdzenie początku zdania
+        if not in_sentence and not is_whitespace(char):
             in_sentence = True
         
+        # Zbieranie znaków zdania
         if in_sentence:
-            sentence += char
+            current_sentence += char
             
-            if is_end_of_sentence(char) or (char == '\n' and i+1 < len(text) and text[i+1] == '\n'):
-                # Koniec zdania
-                sentences.append(sentence.strip())
-                sentence = ""
+            # Sprawdzenie końca zdania
+            if is_end_of_sentence(char) or (char == '\n' and prev_char == '\n'):
+                # Mamy kompletne zdanie
+                if has_proper_name(current_sentence):
+                    proper_name_count += 1
+                total_sentences += 1
+                
+                # Reset dla nowego zdania
+                current_sentence = ""
                 in_sentence = False
+        
+        # Przygotowanie do następnej iteracji
+        prev_char = char
+        char = sys.stdin.read(1)
     
     # Obsługa ostatniego zdania, jeśli nie kończy się znakiem końca zdania
-    if sentence:
-        sentences.append(sentence.strip())
-    
-    # Obliczanie procentu zdań z nazwami własnymi
-    proper_name_count = 0
-    total_sentences = len(sentences)
-    
-    for sentence in sentences:
-        if has_proper_name(sentence):
+    if current_sentence:
+        if has_proper_name(current_sentence):
             proper_name_count += 1
+        total_sentences += 1
     
+    # Obliczanie procentu
     if total_sentences > 0:
         percent = (proper_name_count / total_sentences) * 100
     else:
